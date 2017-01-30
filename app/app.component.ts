30aee611ce80692343bc1a66897e51e30269a730
Tiago Lopes de Paiva Dionysio da Fonseca <tiagoapolo@gmail.com>
2017-01-29 22:40:31 -0200

    init

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-app',
+  template: '<div><h1>{{pageTitle}}</h1></div>'  
+})
+
+export class AppComponent {
+  pageTitle: string = 'Acme'
+}
